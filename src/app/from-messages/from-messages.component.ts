e18d6a2842a9c33d6e72261bc29cc4f33f67a80d
Florian Moritz <Florian.Moritz@evosoft.com>
2020-11-06 14:52:11 +0100

    little bugfixing

--- src/app/from-messages/from-messages.component.ts
+++ src/app/from-messages/from-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ReflectiveInjector } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 @Component({
